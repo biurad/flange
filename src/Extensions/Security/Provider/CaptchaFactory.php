169674abfabb6aa05781b53984afec3ecfff6a26
Divine Niiquaye Ibok <divineibok@gmail.com>
2022-05-26 15:06:24 +0000

    Added a captcha's security factory provider class

---
+++ src/Extensions/Security/Provider/CaptchaFactory.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of DivineNii opensource projects.
+ *
+ * PHP version 7.4 and above required
+ *
+ * @author    Divine Niiquaye Ibok <divineibok@gmail.com>
+ * @copyright 2019 DivineNii (https://divinenii.com/)
+ * @license   https://opensource.org/licenses/BSD-3-Clause License
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Rade\DI\Extensions\Security\Provider;
+
+use Biurad\Security\Handler\CaptchaAuthenticator;
+use Rade\DI\AbstractContainer;
+use Rade\DI\Definition;
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
+
+class CaptchaFactory extends AbstractFactory
+{
+    public function getKey(): string
+    {
+        return 'captcha';
+    }
+
+    public function addConfiguration(NodeDefinition $node): void
+    {
+        $node
+            ->beforeNormalization()
+                ->ifString()->then(fn ($v) => ['recaptcha_secret' => $v])
+            ->end()
+            ->children()
+                ->scalarNode('recaptcha_secret')->defaultNull()->end()
+                ->scalarNode('hcaptcha_secret')->defaultNull()->end()
+            ->end()
+        ;
+    }
+
+    public function create(AbstractContainer $container, string $id, array $config): void
+    {
+        $container->autowire($id, new Definition(CaptchaAuthenticator::class, [$config['recaptcha_secret'], $config['hcaptcha_secret']]));
+    }
+}
